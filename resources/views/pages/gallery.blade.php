db79a522e10259d8fea5ba2a508f708e18477552
unknown <prakash01@Fazil-PC.VACKER.NET>
2017-04-12 14:56:33 +0400

    formated gallery view

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -3,23 +3,25 @@
 @section('content')
     <div class="row">
         <div class="page-header">
-            <h2>My Gallery <i class="fa fa-smile-o" aria-hidden="true"></i></h2>
+            <h2>My Gallery <i class="fa fa-smile-o" aria-hidden="true"></i> </h2>
         </div></div>
+    <div class="row"></div> <i>Pictures used here are either clicked by me or rights taken from photographer.</i</div>
 
 @if (!Auth::guest())
-    <div class="row">
-        @foreach($photoAlbums as $item)
+    @foreach($photoAlbums as $item)
+        <div class="row">
             <?php
                 $folder = "appfiles/photoalbum/" . $item->folder_id.'/';
                 $images = glob($folder."*.*");
-            ?>
-            <h2>{{ucfirst($item->name)}}</h2>
+                ?>
+                <h2>{{ucfirst($item->name)}}</h2>
 
-            @foreach($images as $image)
-                <img height="300" width="300" class="col-sm-4" src="{{ $image }}" />
-            @endforeach
-        @endforeach
-    </div>
+                @foreach($images as $image)
+                    <img height="300" width="300" class="col-sm-4" src="{{ $image }}" />
+                @endforeach
+        </div>
+    @endforeach
+    <br /><br />
 @endif
 
 @endsection
